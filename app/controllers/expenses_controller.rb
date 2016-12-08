237424f621375449669aa92869626bff345c3a6a
eSilvac <esilva1995@gmail.com>
2016-12-08 16:55:15 -0500

    Solucion reto 11

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,5 +1,33 @@
 class ExpensesController < ApplicationController
-  def index
-     @expenses = Expense.order("date DESC")
-  end
+  	def index
+  		if params[:category_id] == nil || params[:category_id] == ""
+     		expenses_temporal = Expense.all
+     		@expenses = Array.new
+     		if params[:concept] == nil || params[:concept] == ""
+     			@expenses = expenses_temporal
+     		else
+	     		expenses_temporal.each do |temp|
+	     			concept = temp.concept.downcase
+	     			if concept.include? params[:concept]
+	     				@expenses << temp
+	     			end
+	     		end
+     		end
+    	else
+     		category = Category.find(params[:category_id])
+     		expenses_temporal = category.expenses
+     		@expenses = Array.new
+     	
+     		if params[:concept] == nil || params[:concept] ==""
+     			@expenses = expenses_temporal
+     		else
+	     		expenses_temporal.each do |temp|
+	     			concept = temp.concept.downcase
+	     			if concept.include? params[:concept]
+	     				@expenses << temp
+	     			end		
+	     		end
+     		end
+    	end
+	end
 end
